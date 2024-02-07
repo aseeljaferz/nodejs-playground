0182f5e88065a959dc56cad77e0e1e6f21da4a27
aseeljaferz <aseeljafer@gmail.com>
2024-02-07 20:48:23 +0530

    5-es6-objects.js es6-object property and destructuring

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,39 @@
+//object property shorthand
+
+const name = 'Aseel';
+const userAge = 22;
+
+const user = {
+    name,
+    age: userAge,
+    location: 'Erode'
+}
+
+console.log(user);
+
+//onject destructuring
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+// const label = product.label;
+// const stock = product.stock;
+
+// //destructuring
+// const { label, stock, rating = 5 } = product;
+// console.log(label);
+// console.log(stock);
+// console.log(rating);
+
+// //renaming variable
+// const { price: productPrice } = product;
+// console.log(productPrice);
+
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock);
+}
+
+transaction('order', product);
