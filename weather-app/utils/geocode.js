debf78cb786f2dfad23ec709530fa9fe74d7c1ea
aseeljaferz <aseeljafer@gmail.com>
2024-02-07 19:38:44 +0530

    geocode was coaded using a callback

---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("postman-request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?access_token=pk.eyJ1IjoiYXNlZWxqYWZlciIsImEiOiJjbDl5anFzMmowNWlsM3JvYnFic3JoenIyIn0.CA2X_GtKlw227T9XJalgow&limit=1";
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location. Try another seatch.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0],
+        location: response.body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
