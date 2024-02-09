76799ac30bfabee35caa51f06f8d9824e51833a8
aseeljaferz <aseeljafer@gmail.com>
2024-02-09 10:32:08 +0530

    built an JSON HTTP endpoint int the web-server for geocode and forecast

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,65 +1,98 @@
 const path = require("path");
 const express = require("express");
-const hbs = require('hbs');
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, '../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials');
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //Setup handlebars engine and views location
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Aseel Jafer'
-    });
-});
-
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About',
-        name: 'Aseel Jafer'
-    });
-});
-
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some Helpful text.',
-        title: 'Help',
-        name: 'Aseel Jafer'
-    })
-})
-
-app.get('/weather', (req, res) => {
-  res.send({
-    forescast: "It is hot",
-    location: "Tamil Nade",
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Aseel Jafer",
   });
 });
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Aseel Jafer', 
-        errorMessage: 'Help article not found.'
-    });
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Aseel Jafer",
+  });
 });
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Aseel Jafer',
-        errorMessage: 'Page not found.'
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some Helpful text.",
+    title: "Help",
+    name: "Aseel Jafer",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
     });
+  }
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+       return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return res.send({ error });
+        }
+    
+        res.send({
+            forecast: forecastData,
+            location,
+            address: req.query.address
+        });
+      });
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+    b;
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Aseel Jafer",
+    errorMessage: "Help article not found.",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Aseel Jafer",
+    errorMessage: "Page not found.",
+  });
 });
 
 app.listen(3000, () => {
